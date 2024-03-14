868deeba92454f8cec5398863a89bf3980c93ee1
nathan <nathanberry97@live.com>
2024-03-14 22:34:57 +0000

    update to how the nav bar works

--- src/js/navBarScroll.js
+++ src/js/navBarScroll.js
@@ -1,39 +1,31 @@
-/*
+/**
  * Purpose:
- *   - This file is used to show/hide the navbar when the user
- *     scrolls up or down the page
+ *   - This file is used to scroll to the top of the page when the user clicks
+ *     on the "to top" button
+ *   - This Button will only be applied to my blog posts
  */
 
-// Set the previous scroll position to the current scroll position
-let prevScrollPos = window.scrollY;
-
 window.onscroll = function () {
-    /*
-     * Get relevant scroll positions
-     */
-    const currentScrollPos = window.scrollY;
-    const bottomPos = document.body.scrollHeight - window.innerHeight - 25;
-    const topPos = 100;
+    const checkOne = document.body.scrollTop > 20;
+    const checkTwo = document.documentElement.scrollTop > 20;
 
-    /*
-     * Check if the user meets one of the following conditions:
-     *   - The user is at the top of the page
-     *   - The user is scrolling up
-     *   - The user is at the bottom of the page
-     */
-    const checkOne = currentScrollPos <= topPos;
-    const checkTwo = prevScrollPos > currentScrollPos;
-    const checkThree = currentScrollPos >= bottomPos;
-
-    /*
-     * If the user meets one of the conditions, show the navbar
-     * Otherwise, hide the navbar
-     */
-    if (checkOne || checkTwo || checkThree) {
-        document.getElementById("navbar").style.top = "0";
+    if (checkOne || checkTwo) {
+        document.getElementById("toTop").style.display = "block";
     } else {
-        document.getElementById("navbar").style.top = "-50px";
+        document.getElementById("toTop").style.display = "none";
     }
-
-    prevScrollPos = currentScrollPos;
 };
+
+function toTop() {
+    // For Safari
+    document.body.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
+
+    // For Chrome, Firefox, IE and Opera
+    document.documentElement.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
+}
