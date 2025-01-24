ac5f99fe328231f7b6a40da187d6af0993c2433f
nathan <nathanberry97@live.com>
2025-01-24 08:02:17 +0000

    updating script to create both index and blog html pages

--- scripts/parseBlogFeed.go
+++ scripts/parseBlogFeed.go
@@ -5,26 +5,40 @@ import (
 	"html/template"
 	"os"
 	"path/filepath"
+	"sort"
 	"strings"
+	"time"
 )
 
 type BlogPost struct {
-	Title string
-	Link  string
-	Date  string
+	Title      string
+	Link       string
+	Date       string
+	ParsedDate time.Time
 }
 
 func main() {
 	blogPosts := getBlogPosts()
-	tmpl := template.Must(template.ParseFiles("templates/index.html"))
+	latestPosts := blogPosts
+	if len(blogPosts) > 3 {
+		latestPosts = latestPosts[:3]
+	}
 
-	outputFile, err := os.Create("scripts/test.html")
+	indexTemplate := template.Must(template.ParseFiles("templates/index.html"))
+	blogTemplate := template.Must(template.ParseFiles("templates/blog.html"))
+
+	createHtml("static/index.html", indexTemplate, latestPosts)
+	createHtml("static/blog.html", blogTemplate, blogPosts)
+}
+
+func createHtml(filePath string, tmpl *template.Template, posts []BlogPost) {
+	outputFile, err := os.Create(filePath)
 	if err != nil {
 		panic(err)
 	}
 	defer outputFile.Close()
 
-	err = tmpl.Execute(outputFile, blogPosts)
+	err = tmpl.Execute(outputFile, posts)
 	if err != nil {
 		panic(err)
 	}
@@ -62,14 +76,24 @@ func getBlogPosts() []BlogPost {
 			}
 		}
 
+		parsedDate, err := time.Parse("2006-01-02", date)
+		if err != nil {
+			panic(err)
+		}
+
 		htmlLink := strings.TrimSuffix(file.Name(), ".md") + ".html"
 
 		blogPosts = append(blogPosts, BlogPost{
-			Title: title,
-			Date:  date,
-			Link:  htmlLink,
+			Title:      title,
+			Date:       date,
+			Link:       htmlLink,
+			ParsedDate: parsedDate,
 		})
 	}
+
+	sort.Slice(blogPosts, func(i, j int) bool {
+		return blogPosts[i].ParsedDate.After(blogPosts[j].ParsedDate)
+	})
 
 	return blogPosts
 }
